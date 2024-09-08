d0ad52753cd8bfffdf19416165be4a2122685e71
Sunset-06 <nilay.sabnis05@gmail.com>
2024-09-09 04:58:36 +0530

    refactored login

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,21 +1,34 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Drawer from "./components/Drawer";
+import Login from "./pages/Login";
 import HomePage from "./pages/HomePage";
-import ShiftPage from "./pages/ShiftPage"
+import ShiftPage from "./pages/ShiftPage";
 import Safety from "./pages/Safety";
 import History from "./pages/History";
 
-const Router = () => {
+const AppRouter = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/";
+
   return (
-    <BrowserRouter>
-     <Drawer />
+    <>
+      {!isLoginPage && <Drawer />}
       <Routes>
-        <Route index element={<HomePage />} />
+        <Route index element={<Login />} />
+        <Route path="/dashboard" element={<HomePage />} />
         <Route path="/shift" element={<ShiftPage />} />
         <Route path="/report" element={<Safety />} />
         <Route path="/history" element={<History />} />
       </Routes>
+    </>
+  );
+};
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <AppRouter />
     </BrowserRouter>
   );
 };
