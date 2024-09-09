fe1167d003759733664e8463e1a6b544db069c1c
MadhavSinha007 <143022751+MadhavSinha007@users.noreply.github.com>
2024-09-09 13:26:37 +0530

    Homepage changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ const HomePage = () => {
     return(
         <>
             <h1>Overview</h1>
+            <img src="src\assets\Screenshot 2024-09-09 132331.png" ></img>
             
         </>
     );
